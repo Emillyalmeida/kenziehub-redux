6927eeaf0c1d68437b07d9f4de1565223f0b1c59
emillyalmeida <emillyalmeidan@gmail.com>
2022-06-13 16:46:47 -0300

    refactor(Dashboard): componentization of the modal

--- src/components/CardTech/index.jsx
+++ src/components/CardTech/index.jsx
@@ -1,8 +1,8 @@
 import { LiCard } from "./style";
 
-const CardTech = ({ title, status, id, open, ...rest }) => {
+const CardTech = ({ title, status, id, isInfo, ...rest }) => {
   return (
-    <LiCard {...rest} onClick={() => open(id)}>
+    <LiCard {...rest} onClick={() => isInfo(id)}>
       <h4>{title}</h4>
       <span>{status}</span>
     </LiCard>
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,46 +1,26 @@
 import { useState } from "react";
 import { useEffect } from "react";
 
-import { useForm } from "react-hook-form";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-
 import Container from "../../components/Container";
 import Logo from "../../components/Logo";
 import CardTech from "../../components/CardTech";
 
 import { NavBar, Header, Main, ListTech } from "./style";
 
-import api from "../../services/api";
+import { useDisclosure } from "@chakra-ui/react";
 
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  useDisclosure,
-  AlertDialogFooter,
-  Button,
-  AlertDialogBody,
-  AlertDialogOverlay,
-  AlertDialogContent,
-  AlertDialogHeader,
-  AlertDialog,
-} from "@chakra-ui/react";
-import { ChakraProvider } from "@chakra-ui/react";
-
-import Buttons from "../../components/Buttons";
-import Input from "../../components/Input";
-import Select from "../../components/Select";
-import { toast } from "react-toastify";
 import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+
 import AddTech from "../../components/Modals/modal-create";
 
-import { PostTechThunk, getTechsThunk } from "../../store/modules/techs/thunk";
+import ModalInfo from "../../components/Modals/modal-info";
+
+import {
+  PostTechThunk,
+  getTechsThunk,
+  patchTechsThunk,
+} from "../../store/modules/techs/thunk";
 
 const Dashboard = () => {
   const listTechs = useSelector(({ techs }) => techs);
@@ -52,18 +32,13 @@ const Dashboard = () => {
     dispatch(getTechsThunk());
   }, []);
 
-  const [token] = useState(
-    JSON.parse(localStorage.getItem("@kenzieHub:token")) || ""
-  );
-
   const [detailTech, setdetail] = useState({});
 
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const { isOpen: open, onOpen: openOn, onClose: closeOn } = useDisclosure();
   const {
-    isOpen: isDelete,
-    onOpen: onDelOpen,
-    onClose: onDelClose,
+    isOpen: isInfo,
+    onOpen: onOpenInfo,
+    onClose: onCloseInfo,
   } = useDisclosure();
 
   const history = useHistory();
@@ -78,53 +53,16 @@ const Dashboard = () => {
     const info = listTechs.find((tec) => tec.id === id);
     console.log(info);
     setdetail(info);
-    openOn();
+    onOpenInfo();
   };
 
   const pacthTech = (data) => {
-    // const { status } = data;
     const pacth = { status: data.status };
     console.log(pacth);
-
-    api
-      .put(`/users/techs/${detailTech.id}`, pacth, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((res) => {
-        console.log(res);
-        toast.success("A tecnologia foi atualizada");
-        // getTechs();
-        closeOn();
-      })
-      .catch((err) => {
-        console.log(err);
-        toast.error("Ops, Houve um erro");
-      });
+    dispatch(
+      patchTechsThunk(pacth, detailTech.id, dataUser.token, onCloseInfo)
+    );
   };
-
-  const deleteTech = () => {
-    api
-      .delete(`/users/techs/${detailTech.id}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((res) => {
-        console.log(res);
-        toast.success("A tecnologia foi excluida");
-        // getTechs();
-        onDelClose();
-        closeOn();
-      })
-      .catch((err) => {
-        console.log(err);
-        toast.error("Ops, não foi possivel excluir");
-      });
-  };
-
-  const { register: regispatch, handleSubmit: handlePatch } = useForm();
 
   return (
     <Container>
@@ -149,79 +87,19 @@ const Dashboard = () => {
                 key={tec.id}
                 title={tec.title}
                 status={tec.status}
-                open={infoTech}
+                isInfo={infoTech}
                 id={tec.id}
               ></CardTech>
             ))
           ) : (
             <h3> Nenhuma tecnologia cadastrada</h3>
           )}
-          <ChakraProvider resetCSS={false}>
-            <Modal isOpen={open} onClose={closeOn}>
-              <ModalOverlay />
-              <ModalContent>
-                <ModalHeader>Tecnologia Detalhes</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody pb={6} component="form">
-                  <Input
-                    register={regispatch}
-                    name="title"
-                    label="Nome do projeto"
-                    placeholder={detailTech.title}
-                    // error={errors.name?.message}
-                    value={detailTech.title}
-                    disabled
-                  />
-
-                  <Select
-                    placeholder={detailTech.status}
-                    // error={errors.status?.message}
-                    register={regispatch}
-                    name="status"
-                    label="Selecione nivel"
-                  >
-                    <option value="Iniciante">Iniciante</option>
-                    <option value="Intermediário">Intermediário</option>
-                    <option value="Avançado">Avançado</option>
-                  </Select>
-                </ModalBody>
-
-                <ModalFooter>
-                  <Buttons whiteTheme onClick={handlePatch(pacthTech)}>
-                    Salvar alterações
-                  </Buttons>
-                  <button className="excluir" onClick={onDelOpen}>
-                    Excluir
-                  </button>
-
-                  <AlertDialog isOpen={isDelete} onClose={onDelClose}>
-                    <AlertDialogOverlay>
-                      <AlertDialogContent>
-                        <AlertDialogHeader fontSize="lg" fontWeight="bold">
-                          Delete listTechs
-                        </AlertDialogHeader>
-
-                        <AlertDialogBody>
-                          Tem certeza que deseja excluir essa tecnologia?
-                        </AlertDialogBody>
-
-                        <AlertDialogFooter>
-                          <Button onClick={onDelClose}>Cancel</Button>
-                          <Button
-                            colorScheme="red"
-                            onClick={() => deleteTech()}
-                            ml={3}
-                          >
-                            Delete
-                          </Button>
-                        </AlertDialogFooter>
-                      </AlertDialogContent>
-                    </AlertDialogOverlay>
-                  </AlertDialog>
-                </ModalFooter>
-              </ModalContent>
-            </Modal>
-          </ChakraProvider>
+          <ModalInfo
+            detailTech={detailTech}
+            isInfo={isInfo}
+            onCloseInfo={onCloseInfo}
+            pacthTech={pacthTech}
+          />
         </ListTech>
       </Main>
     </Container>
