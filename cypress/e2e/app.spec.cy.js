f2bff67929c6a8cdec89be98a6452cba2d397180
emillyalmeida <emillyalmeidan@gmail.com>
2022-07-15 14:11:58 -0300

    test: test e2e add to able to delete, and pacth

--- cypress/e2e/app.spec.cy.js
+++ cypress/e2e/app.spec.cy.js
@@ -3,10 +3,17 @@ import {
   LoginUser,
 } from "../../src/store/modules/userAuth/actions";
 
+import {
+  addTechs,
+  patchTechs,
+  deleteTechs,
+  setTechs,
+} from "../../src/store/modules/techs/actions";
+
 describe("Test KenzieHub", () => {
   const user = {
     user: {
-      id: "4ccdd8a0-f02b-46ad-a275-384ca74b72a5",
+      id: "4ccdd8a0-f02b-46ad-a275-384ca74b1457",
       name: "John Doe",
       email: "johdoe@mail.com",
       course_module: "Terceiro módulo (Introdução ao Backend)",
@@ -20,6 +27,17 @@ describe("Test KenzieHub", () => {
     },
     token: "jhewofheurghiotji6yj",
   };
+
+  const tech = {
+    id: "ad85c1e7-9989-46e1-bc5b-49a24e7cb78",
+    title: "react-native",
+    status: "Avançado",
+    user: { id: "4ccdd8a0-f02b-46ad-a275-384ca74b72a5" },
+    created_at: "2022-07-15T14:35:54.394Z",
+    updated_at: "2022-07-15T14:35:54.394Z",
+  };
+
+  const listTech = [];
 
   const dispatch = (action) =>
     cy.window().its("store").invoke("dispatch", action);
@@ -67,5 +85,82 @@ describe("Test KenzieHub", () => {
 
     dispatch(LoginUser(user));
     cy.contains("Tecnologias");
+  });
+
+  it("tries to add a new tech", () => {
+    cy.viewport(1440, 900);
+    cy.intercept("GET", "/users/4ccdd8a0-f02b-46ad-a275-384ca74b1457", {
+      statusCode: 200,
+      body: [tech],
+    });
+
+    cy.intercept("POST", "/users/techs", {
+      statusCode: 201,
+      body: tech,
+    }).as("createTechs");
+
+    cy.get(".kplSUb div button").click();
+
+    cy.get("input[name=title]").type("react-native");
+    cy.get("select").type("Avançado");
+
+    cy.get(".cNAjwg").click();
+    dispatch(addTechs([...listTech, tech]));
+    cy.contains("react-native");
+  });
+
+  it("able to open modal info", () => {
+    cy.viewport(1440, 900);
+
+    cy.get(".eSISch li:first").click();
+
+    cy.contains("Tecnologia Detalhes");
+  });
+
+  it("able to edit tech", () => {
+    cy.viewport(1440, 900);
+    cy.intercept("GET", "/users/4ccdd8a0-f02b-46ad-a275-384ca74b1457", {
+      statusCode: 200,
+      body: [tech],
+    });
+
+    cy.get("select").type("Iniciante");
+
+    cy.get("footer button:first").click();
+
+    tech.status = "Iniciante";
+    cy.intercept("PUT", "/users/techs/ad85c1e7-9989-46e1-bc5b-49a24e7cb78", {
+      statusCode: 200,
+      body: tech,
+    }).as("pacthTechs");
+
+    dispatch(patchTechs([tech]));
+
+    cy.get(".eSISch").contains("Iniciante");
+  });
+
+  it("should to able to delete tech", () => {
+    cy.viewport(1440, 900);
+    cy.intercept("GET", "/users/4ccdd8a0-f02b-46ad-a275-384ca74b1457", {
+      statusCode: 200,
+      body: [tech],
+    });
+
+    cy.get(".eSISch li:first").click();
+
+    cy.get("footer button:last").click();
+
+    tech.status = "Iniciante";
+    cy.intercept("DELETE", "/users/techs/ad85c1e7-9989-46e1-bc5b-49a24e7cb78", {
+      statusCode: 200,
+    }).as("Techs");
+
+    cy.contains("Tem certeza que deseja excluir essa tecnologia");
+
+    cy.get(".css-cjkud5").click();
+
+    dispatch(deleteTechs([]));
+
+    cy.get("ul > h3").contains("Nenhuma tecnologia cadastrada");
   });
 });
